67436136389ff545ab1acfc9ed016b68fdc48a9a
Niko Ollikainen <nol@it-minds.no>
2023-01-03 13:21:32 +0000

    changed ordering semantics of default manifest

--- Assignments/vagrant/environments/test/manifests/default.pp
+++ Assignments/vagrant/environments/test/manifests/default.pp
@@ -2,27 +2,27 @@
 
 node default {
 
-    # Execute apt-get update
-
-    exec { 'apt-get update':
-        command => '/usr/bin/apt-get update',
-    }
-
-# Ensure curl is installed
-  package { 'curl':
-    require => Exec['apt-get update'],
-    ensure => installed,
+  # Execute apt-get update
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
+    path    => '/usr/bin',
+    before  => Package['curl'],
   }
 
-  #Install node.js
-    exec { 'get-node':
-        require => [Exec['apt-get update'], Package['curl']],
-        command => '/usr/bin/curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
-    }
+  # Ensure curl is installed
+  package { 'curl':
+    ensure => installed,
+    before  => Exec['get-node']
+  }
 
-    package { 'nodejs': 
-        require => Exec['get-node'],
-        ensure => installed,
-    }
+  # Install node.js
+  exec { 'get-node':
+      require => [Exec['apt-get update'], Package['curl']],
+      command => '/usr/bin/curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
+  }
 
+  package { 'nodejs':
+      require => Exec['get-node'],
+      ensure => installed,
+  }
 }
